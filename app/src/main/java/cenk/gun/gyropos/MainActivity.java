4f6435f72b7a83767c3bdb840301ff0db2a0f4ac
Cenk Gün <cenk.gn@gmail.com>
2017-03-04 13:48:11 +0300

    Gyro sensor read.

--- app/src/main/java/cenk/gun/gyropos/MainActivity.java
+++ app/src/main/java/cenk/gun/gyropos/MainActivity.java
@@ -1,13 +1,54 @@
 package cenk.gun.gyropos;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textX, textY, textZ;
+    SensorManager sensorManager;
+    Sensor sensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        textX = (TextView) findViewById(R.id.textX);
+        textY = (TextView) findViewById(R.id.textY);
+        textZ = (TextView) findViewById(R.id.textZ);
     }
+
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroListener, sensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(gyroListener);
+    }
+
+    public SensorEventListener gyroListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int acc) { }
+
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            textX.setText("X : " + (int)x + " rad/s");
+            textY.setText("Y : " + (int)y + " rad/s");
+            textZ.setText("Z : " + (int)z + " rad/s");
+        }
+    };
 }
