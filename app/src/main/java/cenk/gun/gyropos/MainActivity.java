6182de9cd64b1f2aa64a4370caa73b5550da9a2b
Cenk Gün <cenk.gn@gmail.com>
2017-03-09 19:10:40 +0300

    Acelerometer sensor read.

--- app/src/main/java/cenk/gun/gyropos/MainActivity.java
+++ app/src/main/java/cenk/gun/gyropos/MainActivity.java
@@ -12,43 +12,80 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView textX, textY, textZ;
-    SensorManager sensorManager;
-    Sensor sensor;
+    TextView tv_accX, tv_accY, tv_accZ;
+    SensorManager gyroManager, accManager;
+    Sensor gyroSensor, accSensor;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        gyroManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gyroSensor = gyroManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        accManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accSensor = accManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         textX = (TextView) findViewById(R.id.textX);
         textY = (TextView) findViewById(R.id.textY);
         textZ = (TextView) findViewById(R.id.textZ);
+
+        tv_accX = (TextView) findViewById(R.id.accX);
+        tv_accY = (TextView)findViewById(R.id.accY);
+        tv_accZ = (TextView) findViewById(R.id.accZ);
+
+
     }
 
     public void onResume() {
         super.onResume();
-        sensorManager.registerListener(gyroListener, sensor,
+        gyroManager.registerListener(gyroListener, gyroSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        accManager.registerListener(accListener, accSensor,
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     public void onStop() {
         super.onStop();
-        sensorManager.unregisterListener(gyroListener);
+        gyroManager.unregisterListener(gyroListener);
+        accManager.unregisterListener(accListener);
+
     }
 
     public SensorEventListener gyroListener = new SensorEventListener() {
-        public void onAccuracyChanged(Sensor sensor, int acc) { }
+        public void onAccuracyChanged(Sensor sensor, int acc) {
+        }
 
         public void onSensorChanged(SensorEvent event) {
             float x = event.values[0];
             float y = event.values[1];
             float z = event.values[2];
 
-            textX.setText("X : " + (int)x + " rad/s");
-            textY.setText("Y : " + (int)y + " rad/s");
-            textZ.setText("Z : " + (int)z + " rad/s");
+            textX.setText("X : " + x + " rad/s");
+            textY.setText("Y : " + y + " rad/s");
+            textZ.setText("Z : " + z + " rad/s");
+        }
+    };
+
+    public SensorEventListener accListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            tv_accX.setText("X Acc : " + x + " m/s2");
+            tv_accY.setText("Y Acc: " + y + " m/s2");
+            tv_accZ.setText("Z Acc: " + z + " m/s2");
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
         }
     };
 }
